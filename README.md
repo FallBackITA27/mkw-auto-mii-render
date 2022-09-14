b498d5261c21e2c526f41bfbbb298d62228e3154
FallBackITA27 <49619073+FallBackITA27@users.noreply.github.com>
2022-09-14 18:04:19 +0200

    Commented the whole thing.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This project is meant to one day be merged with https://github.com/GiGGioSo/mkwii-ita-auto-TT-boards.
 
+Currently doesn't work, I wouldn't suggest trying to run it.
+
 Thought I'd put it up for a while.
--- miigx.py
+++ miigx.py
@@ -1,5 +1,4 @@
 import os
-from struct import pack
 from mii2studio import output
 from time import sleep
 
@@ -11,8 +10,10 @@ def echo(x):
     print(x)
     return x
 
+# I have yet to finish it
 print("Please read the ReadMe first!")
 
+#Creates Files folder, a temp folder inside of it, the output folder and the output folder miigx's
 try:
     os.mkdir("files")
 except:
@@ -30,17 +31,20 @@ try:
 except:
     pass
 
-rkgfiles = os.listdir("files")
-
+# chose input
 first_input = input("Y for Normal \"Read Files from Folder\" mode\nP for \"Paste bytes\" mode\nQ for quit\n >> ").lower()
+# go to line 77
 
 def isRKG(x:str):
+    # filter for the /files/ folder.
     if x.endswith(".rkg") is True:
         return x
     if x.endswith(".RKG") is True:
         return x
 
 def read_bytes(wbytes):
+    # Currently unfinished.
+    quit()
     if "," in wbytes:
         filenum = 0
         wbytes = wbytes.split(",")
@@ -56,27 +60,36 @@ def read_bytes(wbytes):
     return ""    
 
 def read_rkg(rkgfiles):
+    # filters out any non-.rgk file
     rkgfiles = list(filter(isRKG,rkgfiles))
+    # num for the file name
     num = 1
     for x in rkgfiles:
+        # opens the rkg and writes the miidata in the temp folder
         with open(f"files/{x}","rb") as rf, open(f"files/temp/{num}.miigx","wb") as wf:
+            # 0xC3 Offset | 0x4A Length | Blocks 0xC3 to 0x85
             rf.seek(60)
             rkgd = rf.read(74)
             wf.write(rkgd)
+        # calls Mii2Studio
         output(f"files/temp/{num}.miigx",num)
         num += 1
     
+    # moves the temp file to the completed folder; I wanna move this line on the Mii2Studio.py script and make the file name {mii_name}.miigx 
     os.rename(f"files/temp/{num}.miigx",f"files/temp/{num}.miigx")
+    # remove temp dir
     os.rmdir("files/temp")
     sleep(10)
     quit()
 
-# continuation of inputs after the start
+# continuation of input after the start
 
 match first_input:
-    case "y" : 
+    case "y" :
+        second_input = input("Press enter when you put the files in the file folder.")
+        # reads the files in the files folder
+        rkgfiles = os.listdir("files")
         read_rkg(rkgfiles)
-        quit()
     case "p" :
         print("Paste now.\nPut commas between Miis if you have more than one.")
         inp = input().strip("\n").strip(" ").strip("")
